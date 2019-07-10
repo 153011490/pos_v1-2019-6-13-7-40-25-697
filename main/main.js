af09fb99deecf32a1699ead17c94569b77eeb7e3
SamsonLin <153011490@qq.com>
2019-07-10 21:01:43 +0800

    add a function named isBarCodesValid and do it test

--- main/main.js
+++ main/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+const isBarCodesValid = (barcodes) => {
+    let items=loadAllItems();
+    for(let i=0;i<barcodes.length;i++){
+        for(let j=0;j<items.length;j++){
+            if(barcodes[i].substr(0,10)==items[j].barcode){
+                return true;
+            }
+        }
+    }
+    return false;
+}
