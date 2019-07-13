424e780bb522b60ca94dfddbd435f94879b1ff03
SamsonLin <153011490@qq.com>
2019-07-13 15:04:07 +0800

    feat:add new function getItemList
    add a new function getItemList and pass it in test

--- main/main.js
+++ main/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
 const isBarCodesValid = (barcodes) => {
     let items=loadAllItems();
     for(let i=0;i<barcodes.length;i++){
@@ -12,3 +11,13 @@ const isBarCodesValid = (barcodes) => {
     }
     return false;
 }
+
+const getItemList = (barcodes) => {
+    let itemList=[];
+    let items=loadAllItems();
+    let map=barcodes.reduce((m,element)=>m.set(element.substr(0,10),(m.get(element.substr(0,10))||0)+parseFloat(element.indexOf('-')!=-1?element.substr(11):1)),new Map());
+    for (let [key, value] of map) {
+        itemList.push({"item":items.filter(item=>item.barcode==key)[0],"count":value});
+    }
+    return itemList;
+    }
--- test/main-test.js
+++ test/main-test.js
@@ -13,7 +13,15 @@ describe('pos', () => {
   ];
 
   it('should return true', () => {
-    expect(isBarCodesValid(tags)).toBe(true);
+    expect(isBarCodesValid(tags)).toEqual(true);
+   })
+
+   it('should return true', () => {
+     let expected=[{"item":{barcode: 'ITEM000001',name: '雪碧',unit: '瓶',price: 3.00},"count":5},
+     {"item":{barcode: 'ITEM000003',name: '荔枝',unit: '斤',price: 15.00},"count":2.5},
+     {"item": {barcode: 'ITEM000005',name: '方便面',unit: '袋',price: 4.50},"count":3}
+    ];
+    expect(getItemList(tags)).toEqual(expected);
    })
 
   });
